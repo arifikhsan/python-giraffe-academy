ee6f24d75912f31ae8443ef5c49bb1faea14cfb4
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-16 23:47:53 +0700

    number

---
+++ number.py
@@ -0,0 +1,23 @@
+print(2)
+print(2.08)
+print(-2.34)
+print(3 + 4)
+print(3 + 4.5)
+print(3 - 4.5)
+print(12 / 5)
+print(3 * 4.5)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+print(10 % 3)
+print(str(5) + ' my favorite number')
+print(abs(-5))
+print(pow(5, 2))
+print(max(4, 6)) # bigger number
+print(min(4, 6)) # smallest number
+print(round(2.2))
+print(round(6.7))
+
+from math import *
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
